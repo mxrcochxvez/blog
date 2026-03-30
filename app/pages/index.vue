9be2290f5c84e42cefefd467f3f38826bfde590a
Marco Chavez <marcode.chavez.jr@gmail.com>
2026-03-29 22:53:38 -0700

    feat: create landing page to display blog post list from content collection

---
+++ app/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="home-container">
+    <h1>Marco's Blog</h1>
+    
+    <ul>
+      <li v-for="{ path, title } in blogPosts">
+        <NuxtLink :to="path">
+          {{ title }}
+        </NuxtLink>
+      </li>
+    </ul>
+  </main>
+</template>
+
+<script setup lang="ts">
+  const { data: blogPosts } = await useAsyncData('posts', () => queryCollection('blog').all());
+</script>
