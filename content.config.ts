11d53b5c50bbd1c5b30ba1dec140d8901d3de18c
Marco Chavez <marcode.chavez.jr@gmail.com>
2026-03-29 22:53:07 -0700

    refactor: changes content to blogs to better match the intention

--- content.config.ts
+++ content.config.ts
@@ -1,10 +1,10 @@
-import { defineContentConfig, defineCollection } from '@nuxt/content'
+import {defineContentConfig, defineCollection} from '@nuxt/content'
 
 export default defineContentConfig({
   collections: {
-    content: defineCollection({
+    blog: defineCollection({
       type: 'page',
-      source: '**',
+      source: 'blog/**/*.md',
     }),
   },
 })
---
+++ content/blog/my-first-blog.md
@@ -0,0 +1,5 @@
+# My first blog post
+
+This is my first blog post. I'm excited to share my thoughts and ideas with you.
+
+[Go Back](/)
