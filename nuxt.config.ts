b005d4c67fb4c4abc4c641e84420a0a2f28b470a
Marco Chavez <marcode.chavez.jr@gmail.com>
2026-03-29 23:02:08 -0700

    feat: defines environment variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   modules: [
     '@nuxt/content',
   ],
-  devtools: { enabled: true },
+  devtools: {enabled: true},
   compatibilityDate: '2024-04-03',
+  runtimeConfig: {
+    USER_PASS: process.env.USER_PASS,
+  }
 })
